18880a1949942784028300e5db9e6d9c065cf80c
MattTriesCode <25834648MattTriesCode@users.noreply.github.com>
2017-07-25 20:19:29 -0400

    delete all files

--- header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE HTML>
-<html lang='en'>
-<head>
-  <title>Including some files</title>
-</head>
-<body>
-
-  <h1>A site about including some files.</h1>
--- menu.php
+++
@@ -1,6 +0,0 @@
-<ul>
-  <li>Home</li>
-  <li>Menu One</li>
-  <li>Menu Two</li>
-  <li>About</li>
-</ul>
